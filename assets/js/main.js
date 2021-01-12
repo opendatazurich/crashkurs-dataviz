fbb1c57274c9ec104e289dd40f6e552e5d27f6ae
Stefan Oderbolz <oderbolz@gmail.com>
2021-01-12 16:33:53 +0100

    Move to position

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@ $(function() {
   });
 
   $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 150;
+    var position = $($(this).attr("href")).offset().top - 140;
     $("html, body").animate({scrollTop: position}, 400);
     $("nav ul li a").parent().removeClass("active");
     $(this).parent().addClass("active");
